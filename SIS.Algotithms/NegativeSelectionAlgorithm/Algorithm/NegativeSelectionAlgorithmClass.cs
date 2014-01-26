5ec09c4646e01a236a3b1d381a97baae0841848d
flash160 <flash160@wp.pl>
2014-01-26 12:03:35 +0100

    taka sytuacja

---
+++ SIS.Algotithms/NegativeSelectionAlgorithm/Algorithm/NegativeSelectionAlgorithmClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SIS.Algotithms.NegativeSelectionAlgorithm.Population;
+
+namespace SIS.Algotithms.NegativeSelectionAlgorithm.Algorithm
+{
+    public class NegativeSelectionAlgorithmClass
+    {
+        #region variables
+        //jebać gita
+        public Cell stemCell;          //komórka macierzysta (wzorcowa, antygen)
+        public List<Cell> antibodies = new List<Cell>();  //komórki przeciwciał
+        private double tollerance;      //tolerancja odchylenia wyrażona w liczbie od 0 do 1 (procenty) 
+        private int ammountOfEpochs = 0;    //liczba epok jakie były potrzebne do znalezienia rozwiązania
+
+        #endregion
+
+        #region constructors
+
+        public NegativeSelectionAlgorithmClass(int min, int max, int amountOfAntibodies, double tollerance)
+        {
+            try
+            {
+                if (tollerance > 0 && tollerance < 1)
+                {
+                    this.tollerance = tollerance;
+                }
+                else
+                {
+                    throw new Exception("Nie prawidłowa wartość tolerancji");
+                }
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+            this.stemCell = new Cell(min, max);
+            runAlgorithm(amountOfAntibodies, min, max);
+
+        }
+
+        #endregion
+
+        #region methods
+
+        private void runAlgorithm(int amountOfAntibodies, int a, int b)
+        {
+            while (antibodies.Count() <= amountOfAntibodies)
+            {
+                System.Threading.Thread.Sleep(30);
+                Cell tempCell = new Cell(a,b);
+                if (tempCell.getCellValueOfFunction() > stemCell.getCellValueOfFunction() - stemCell.getCellValueOfFunction()
+                    * tollerance && tempCell.getCellValueOfFunction() < stemCell.getCellValueOfFunction() +
+                    stemCell.getCellValueOfFunction() * tollerance)
+                {
+                    antibodies.Add(tempCell);
+                    ammountOfEpochs++;
+                }
+                else
+                {
+                    ammountOfEpochs++;
+                }
+            }
+        }
+
+        public Cell getStemCell()
+        {
+            return stemCell;
+        }
+
+        public int getAmmountOfEpochs()
+        {
+            return ammountOfEpochs;
+        }
+
+        public List<Cell> getAntibodies()
+        {
+            return antibodies;
+        }
+        #endregion
+    }
+}
---
+++ SIS.Algotithms/NegativeSelectionAlgorithm/Population/Cell.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS.Algotithms.NegativeSelectionAlgorithm.Population
+{
+    public class Cell
+    {
+        #region variables
+
+        private int cellValue;      //wartość komórki 
+        private double cellValueOfFunction; //wartość funkcji przystosowania komórki
+        private int a;      //początek przedziału wartości komórki
+        private int b;      //koniec przedziału wartości komórki
+
+        #endregion
+
+        #region constructors
+
+        public Cell(int start, int end)
+        {
+            Random randNum = new Random();
+            cellValue = randNum.Next(start, end);
+            cellValueOfFunction = calculateValueOfCell();
+        }
+
+ 
+        #endregion
+
+
+        #region methods
+
+        private double calculateValueOfCell()
+        {
+            return cellValue * cellValue + 2 * cellValue + 10;
+        }
+
+        public double getCellValueOfFunction()
+        {
+            return cellValueOfFunction;
+        }
+
+        public int getCellValue()
+        {
+            return cellValue;
+        }
+
+        #endregion
+    }
+}
